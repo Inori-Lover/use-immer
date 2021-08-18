9a46e695c5e2971b04df88cc6e6657aec36d457d
inori-lover <337938188@qq.com>
2021-08-19 06:14:38 +0800

    feat: 函数声明重载

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export function useImmer<T>(init?: T) {}
+import { Dispatch, SetStateAction } from "react";
+
+function useImmer<S>(): [
+  S | undefined,
+  Dispatch<SetStateAction<S | undefined>>
+];
+function useImmer<S>(init: S | (() => S)): [S, Dispatch<SetStateAction<S>>];
+
+function useImmer<S>(init?: S | (() => S)) {
+  return [] as any;
+}
+
+export { useImmer };
