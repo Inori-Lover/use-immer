c82403806819f4dbba6793f59592779dd816ce38
inori_lover <337938188@qq.com>
2021-08-21 07:25:01 +0800

    feat: 初步实现

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,31 @@
-import { Dispatch, SetStateAction } from "react";
+import produce, { enableMapSet } from "immer";
+import { Dispatch, useReducer, useState, useCallback } from "react";
 
-function useImmer<S>(): [
-  S | undefined,
-  Dispatch<SetStateAction<S | undefined>>
-];
-function useImmer<S>(init: S | (() => S)): [S, Dispatch<SetStateAction<S>>];
+enableMapSet();
 
-function useImmer<S>(init?: S | (() => S)) {
-  return [] as any;
+export type SetStateAction<S> = S | ((prevState: S) => S | void);
+
+/** immer 版的useState，因为immer的存在所以有一些限制与增强 */
+function useImmer<S>(init: S | (() => S)): [S, Dispatch<SetStateAction<S>>] {
+  const [initState] = useState(init);
+
+  const reducer = useCallback((prevState: S, action: SetStateAction<S>): S => {
+    let nextState = prevState;
+    if (action instanceof Function) {
+      nextState = produce(prevState, action);
+    } else {
+      nextState = action;
+    }
+
+    return nextState;
+  }, []);
+  const [state, dispatch] = useReducer(reducer, initState);
+
+  const setState: Dispatch<SetStateAction<S>> = useCallback((setAction) => {
+    dispatch(setAction);
+  }, []);
+
+  return [state, setState];
 }
 
 export { useImmer };
