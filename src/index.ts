32fd1265de223cb485dc4d3eac3757a3d10d6b6a
inori-lover <337938188@qq.com>
2021-08-24 23:23:45 +0800

    feat: 支持查询patches信息方便做变更检查

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,39 @@
-import produce, { enableMapSet } from "immer";
-import { Dispatch, useReducer, useState, useCallback } from "react";
+import produce, {
+  enableMapSet,
+  enablePatches,
+  applyPatches,
+  Patch,
+} from "immer";
+import {
+  Dispatch,
+  useReducer,
+  useState,
+  useCallback,
+  useRef,
+  useMemo,
+} from "react";
 
 enableMapSet();
+enablePatches();
 
 /** 可以返回nextState也可以原地修改然后不返回 */
 export type SetStateAction<S> = S | ((prevState: S) => S | void);
 
+/** 返回immer相关增强功能 */
+export interface ImmerActions {
+  /** 返回patch数组，长度为0时则代表数据(即使被多次修改后)相比初始值没变化 */
+  patches: () => Patch[];
+}
+
 /** immer 版的useState，因为immer的存在所以有一些限制与增强 */
-function useImmer<S>(init: S | (() => S)): [S, Dispatch<SetStateAction<S>>] {
+function useImmer<S>(
+  init: S | (() => S)
+): [S, Dispatch<SetStateAction<S>>, ImmerActions] {
   const [initState] = useState(init);
+  /** 记录patch信息数组 */
+  const patchesRef = useRef<Patch[]>([]);
+  /** patch信息数组在上次压缩后是否被修改过 */
+  const dirtyMarkRef = useRef(false);
 
   if (typeof initState === "function") {
     console.warn(
@@ -22,11 +47,18 @@ function useImmer<S>(init: S | (() => S)): [S, Dispatch<SetStateAction<S>>] {
     if (action instanceof Function) {
       // 经过校验，setState的行为就是这样：
       // 就算useState里传入的是function，setState只传func也是会运行并取返回值作为nextState
-      nextState = produce(prevState, action);
+      nextState = produce(prevState, action, (patches) =>
+        patchesRef.current.push(...patches)
+      );
     } else {
-      nextState = action;
+      nextState = produce(
+        prevState,
+        () => action,
+        (patches) => patchesRef.current.push(...patches)
+      );
     }
 
+    dirtyMarkRef.current = true;
     return nextState;
   }, []);
 
@@ -36,7 +68,29 @@ function useImmer<S>(init: S | (() => S)): [S, Dispatch<SetStateAction<S>>] {
     dispatch(setAction);
   }, []);
 
-  return [state, setState];
+  const immerActions: ImmerActions = useMemo(
+    () => ({
+      patches() {
+        if (dirtyMarkRef.current) {
+          /** 通过 immer.applyPatches 将patch信息尽可能压缩 */
+          produce(
+            initState,
+            (draft) => {
+              applyPatches(draft, patchesRef.current);
+            },
+            (compressedPatches) => {
+              patchesRef.current = compressedPatches;
+            }
+          );
+          dirtyMarkRef.current = false;
+        }
+        return patchesRef.current;
+      },
+    }),
+    [initState]
+  );
+
+  return [state, setState, immerActions];
 }
 
 export { useImmer };
